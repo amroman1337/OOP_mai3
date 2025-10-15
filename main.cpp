7ace339a1f397c52dbf9a62521e306c6273583fd
Roman Matyushkin <144627459+amroman1337@users.noreply.github.com>
2025-10-16 02:36:57 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,33 +6,33 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    FigureArray figureArray;
+    FigureArray arr;
     int key;
     std::cout << "Лабораторная работа номер 3, вариант 33"<< std::endl;
     while (true) {
         std::cout << "\n Меню: " << std::endl;
-        std::cout << "1 добавить фигуру" << std::endl;
-        std::cout << "2 удалить фигуру по индексу" << std::endl;
-        std::cout << "3 показать все фигуры" << std::endl;
-        std::cout << "4 вычислить общую площадь" << std::endl;
-        std::cout << "5 Bыход" << std::endl;
+        std::cout << "добавить фигуру - 1" << std::endl;
+        std::cout << "удалить фигуру по индексу - 2" << std::endl;
+        std::cout << "показать все фигуры - 3" << std::endl;
+        std::cout << "вычислить общую площадь - 4" << std::endl;
+        std::cout << "Bыход - 5" << std::endl;
         std::cout << "выберите действие: " << std::endl;
         std::cin >> key;
         
         switch (key) {
             case 1: {
-                std::cout << "Выберите тип фигуры: " << std::endl;
+                int f;
+                std::cout << "Bыберите тип фигуры " << std::endl;
                 std::cout << "1-Треугольник " << std::endl;
                 std::cout << "2-Квадрат "<< std::endl;
                 std::cout << "3-Прямоугольник " << std::endl;
                 std::cout << "Ваш выбор: " << std::endl;
-                int figureType;
-                std::cin >> figureType;
+                std::cin >> f;
                 
-                auto figure=createFig(figureType);
-                if (figure) {
-                    std::cin >> *figure;
-                    figureArray.addFig(std::move(figure));
+                auto fig=createFig(f);
+                if (fig) {
+                    std::cin >> *fig;
+                    arr.addFig(std::move(fig));
                     std::cout << "фигура добавлена!" << std::endl;
                 } 
                 else {
@@ -41,28 +41,28 @@ int main() {
                 break;
             }
             case 2: {
-                if (figureArray.size()==0) {
+                int i;
+                if (arr.size()==0) {
                     std::cout << "Mассив фигур пуст" << std::endl;;
                     break;
                 }
-                std::cout << "Bведите индекс фигуры для удаления (1-" << figureArray.size() << "): ";
-                int index;
-                std::cin >> index;
-                figureArray.removeFig(index-1);
+                std::cout << "Bведите индекс фигуры для удаления (1-" << arr.size() << "): ";
+                std::cin >> i;
+                arr.removeFig(i-1);
                 std::cout << "фигура успешно удалена" << std::endl;
                 break;
             }
             case 3: {
-                if (figureArray.size()==0) {
+                if (arr.size()==0) {
                     std::cout << "Mассив фигур пуст" << std::endl;
                 } 
                 else {
-                    figureArray.printAll();
+                    arr.printAll();
                 }
                 break;
             }
             case 4: {
-                double totalArea=figureArray.getTotalArea();
+                double totalArea=arr.getTotalArea();
                 std::cout << "0бщая площадь фигур: " << totalArea << std::endl;
                 break;
             }
